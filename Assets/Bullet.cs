a18f96425e76910c23ce424a74c4dc6a8898c8f0
Vu Phong <bcs230094@st.cmcu.edu.vn>
2026-01-27 12:13:10 +0700

    Demo part2

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed; // Tốc độ bay
+
+    void Update()
+    {
+        // 1. Lấy vị trí hiện tại
+        var newPosition = transform.position;
+
+        // 2. Tăng giá trị Y để bay lên trên
+        newPosition.y += Time.deltaTime * flySpeed; // [cite: 664]
+
+        // 3. Cập nhật lại vị trí
+        transform.position = newPosition; // [cite: 665]
+    }
+}
---
+++ Assets/Playershooting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MyPlayerShooting : MonoBehaviour
+{
+    float spacing = 0.15f;
+    public GameObject bulletPrefab;
+    public float shootingInterval = 0.1f; // Thời gian giãn cách giữa 2 viên đạn [cite: 876]
+    private float lastBulletTime;
+
+    void Update()
+    {
+        // Đổi thành GetMouseButton (giữ chuột) thay vì Down (nhấn 1 cái)
+        if (Input.GetMouseButton(0)) // [cite: 887]
+        {
+            // Kiểm tra thời gian để không bắn quá nhanh
+            if (Time.time - lastBulletTime > shootingInterval) // [cite: 889]
+            {
+                ShootBullet();
+                lastBulletTime = Time.time; // [cite: 896]
+            }
+        }
+    }
+
+    void ShootBullet()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation); // [cite: 902]
+        Vector3 leftPos = transform.position;
+        leftPos.x -= spacing; // Trừ bớt trục X để sang trái
+        Instantiate(bulletPrefab, leftPos, transform.rotation);
+
+        // 3. Viên PHẢI (Lấy vị trí gốc, dịch sang phải một chút)
+        Vector3 rightPos = transform.position;
+        rightPos.x += spacing; // Cộng thêm trục X để sang phải
+        Instantiate(bulletPrefab, rightPos, transform.rotation);
+    }
+}
